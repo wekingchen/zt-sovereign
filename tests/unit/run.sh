720a51348ab59df216b2b57d1e8b04f50e2cc925
wekingchen <weking.chen@gmail.com>
2026-09-21 02:40:02 +0800

    test(ztncui): run modern UI regression checks

--- tests/unit/run.sh
+++ tests/unit/run.sh
@@ -5,4 +5,5 @@ cd "$ROOT"
 ./tests/unit/test_planetctl.sh
 python3 ./tests/unit/test_file_server.py
 python3 ./tests/unit/test_ztncui_zh_cn.py
+python3 ./tests/unit/test_ztncui_modern_ui.py
 ./tests/unit/test_version_sync.sh
